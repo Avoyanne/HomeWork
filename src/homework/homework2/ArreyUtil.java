555977144e4a04ff9b8a4c5f8e9cc8e480d5647a
avoyan fergen <avoyan.ferg03@gmail.com>
2023-01-08 20:17:15 +0400

    homework2

--- src/homework/homework2/ArreyUtil.java
+++ src/homework/homework2/ArreyUtil.java
@@ -18,11 +18,46 @@ public class ArreyUtil {
         System.out.println(a);
         //տպել ամենափոքր թիվը
         int b = arrey[0];
-        for (int i = 0; i <arrey.length; i++) {
+        for (int i = 0; i < arrey.length; i++) {
             if (b > arrey[i]) {
-                b=arrey[i];
+                b = arrey[i];
             }
         }
         System.out.println(b);
+//տպել միայն կենտ թվերը
+        int c = arrey[0];
+        for (int i = 0; i < arrey.length; i++) {
+            if (arrey[i] % 2 != 0) {
+                c = arrey[i];
+                System.out.print(c + " ");
+            }
+        }
+        System.out.println();
+//տպել միայն զույգ թվերը
+        int y = arrey[0];
+        for (int i = 0; i < arrey.length; i++) {
+            if (arrey[i] % 2 == 0) {
+                y = arrey[i];
+                System.out.print(y + " ");
+            }
+
+        }
+        System.out.println();
+//տպել զույգերի քանակը
+        int v = 0;
+        for (int i : arrey) {
+            if (i % 2 == 0) {
+                v++;
+            }
+        }
+        System.out.println(v);
+//տպել կենտերի քանակը
+        int x = 0;
+        for (int i : arrey) {
+            if (i %2 != 0){
+                x++;
+            }
+        }
+        System.out.println(x);
     }
 }
